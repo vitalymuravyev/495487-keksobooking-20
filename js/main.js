c99d4af7d473600564168b45bf79517f7c224c26
vitalymuravyev <vitalymuravyev@gmail.com>
2020-06-10 23:03:46 +0300

    correct pins

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 'use strict';
-var AVATARS = ['01', '02', '03', '04', '05', '06', '07', '08'];
+var MIN_Y = 130;
+var MAX_Y = 630;
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
+var NUMBER_OF_PINS = 8;
+
 var HOME_TYPE = ['palace', 'flat', 'house', 'bungalo'];
 var HOME_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var CHECK_TIME = ['12:00', '13:00', '14:00'];
@@ -8,74 +13,91 @@ var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
 
 var map = document.querySelector('.map');
 
-function renderValue(min, max) {
+function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-function renderOriginal(arr) {
-  var temp;
-  var tempArr = arr;
+function createRandomArray(arr) {
   var newArr = [];
-  var quantity = renderValue(1, arr.length);
+  var quantity = getRandomNumber(0, arr.length - 1);
+
   for (var i = 0; i < quantity; i++) {
-    temp = tempArr.splice(renderValue(0, tempArr.length), 1).toString();
-    newArr.push(temp);
+    newArr.push(arr[i]);
   }
   return newArr;
 }
+function createAvatarsArray() {
+  var tempArr = [];
+  for (var g = 1; g <= NUMBER_OF_PINS; g++) {
+    tempArr.push('img/avatars/user0' + g + '.png');
+  }
+  return tempArr;
+}
+
+var AVATARS = createAvatarsArray();
+
 
 function renderAvatar() {
-  return AVATARS.splice(renderValue(0, AVATARS.length), 1).toString();
+  return AVATARS.splice(getRandomNumber(0, AVATARS.length), 1);
 }
 
 function createPost() {
+  var location = {
+    x: getRandomNumber(0, map.offsetWidth),
+    y: getRandomNumber(MIN_Y, MAX_Y),
+  };
+
   return {
     author: {
-      avatar: 'img/avatars/user' + renderAvatar() + '.png',
+      avatar: renderAvatar(),
     },
 
     offer: {
       title: 'Here will be title of the real post',
       address: location.x + ', ' + location.y,
-      price: renderValue(5, 80) * 1000,
-      type: HOME_TYPE[renderValue(0, HOME_TYPE.length)],
-      rooms: renderValue(1, 4),
-      guests: renderValue(2, 8),
-      checkinn: CHECK_TIME[renderValue(0, CHECK_TIME.length)],
-      checkout: CHECK_TIME[renderValue(0, CHECK_TIME.length)],
-      features: renderOriginal(HOME_FEATURES),
-      photos: renderOriginal(PHOTOS),
+      price: getRandomNumber(5, 80) * 1000,
+      type: HOME_TYPE[getRandomNumber(0, HOME_TYPE.length)],
+      rooms: getRandomNumber(1, 4),
+      guests: getRandomNumber(2, 8),
+      checkinn: CHECK_TIME[getRandomNumber(0, CHECK_TIME.length)],
+      checkout: CHECK_TIME[getRandomNumber(0, CHECK_TIME.length)],
+      features: createRandomArray(HOME_FEATURES),
+      photos: createRandomArray(PHOTOS),
       location: {
-        x: renderValue(0, map.offsetWidth),
-        y: renderValue(130, 630),
+        x: location.x,
+        y: location.y,
       },
     },
   };
 }
 
-var posts = [];
-for (var j = 0; j < 8; j++) {
-  posts.push(createPost());
+function createPosts() {
+  var posts = [];
+  for (var j = 0; j < NUMBER_OF_PINS; j++) {
+    posts.push(createPost());
+  }
+  return posts;
 }
 
+var newPosts = createPosts();
 map.classList.remove('map--faded');
 
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
 function renderPin(post) {
   var pin = pinTemplate.cloneNode(true);
-  pin.style.left = post.offer.location.x + 25 + 'px';
-  pin.style.top = post.offer.location.y + 70 + 'px';
+
+  pin.style.left = post.offer.location.x - PIN_WIDTH / 2 + 'px';
+  pin.style.top = post.offer.location.y - PIN_HEIGHT + 'px';
   pin.querySelector('img').src = post.author.avatar;
   pin.querySelector('img').alt = post.offer.title;
-
   return pin;
 }
 
 var fragment = document.createDocumentFragment();
 
-for (var k = 0; k < 8; k++) {
-  fragment.appendChild(renderPin(posts[k]));
+for (var k = 0; k < NUMBER_OF_PINS; k++) {
+  fragment.appendChild(renderPin(newPosts[k]));
 }
 var mapPins = map.querySelector('.map__pins');
 mapPins.appendChild(fragment);
